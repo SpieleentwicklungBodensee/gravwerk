a12a88c3cfb15f64c3ce7f39e5d07ea4920d4ab0
drwuro <email@drwuro.com>
2021-11-20 15:26:13 +0100

    added initial sprite

---
+++ gamestate.py
@@ -0,0 +1,4 @@
+class GameState():
+    def __init__(self):
+        self.objects = {}
+
---
+++ playerobject.py
@@ -0,0 +1,11 @@
+from globalconst import *
+from gameobjects import *
+
+class PlayerObject(GameObject):
+    def __init__(self, x, y, tile=None):
+        GameObject.__init__(self, x, y, tile)
+
+    def update(self, gamestate):
+        self.x += self.xdir * self.speed
+        self.y += self.ydir * self.speed
+
