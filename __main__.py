383f9c6e549bc34572b55596ad7880eed53ddd3d
drwuro <email@drwuro.com>
2021-11-20 14:52:11 +0100

    added initial code from snake_soccer

---
+++ __main__.py
@@ -0,0 +1,198 @@
+import pygame
+import argparse
+import io
+import os
+import random
+import math
+
+from bitmapfont import BitmapFont
+
+from globalconst import *
+
+
+actions = []
+objects = {}
+
+pygame.display.init()
+
+if FULLSCREEN:
+    window = pygame.display.set_mode(pygame.display.list_modes()[0], pygame.FULLSCREEN)
+else:
+    window = pygame.display.set_mode((WIN_W, WIN_H), 0)
+
+screen = pygame.Surface((SCR_W, SCR_H))
+
+clock = pygame.time.Clock()
+
+pygame.mixer.init(44100)
+pygame.joystick.init()
+
+for i in range(pygame.joystick.get_count()):
+    pygame.joystick.Joystick(i).init()
+
+pygame.mouse.set_visible(False)
+
+font = BitmapFont('gfx/heimatfont.png', scr_w=SCR_W, scr_h=SCR_H, colors=[(255,255,255), (240,0,240)])
+
+
+
+def toggleFullscreen():
+    global FULLSCREEN, window
+    FULLSCREEN = not FULLSCREEN
+    if FULLSCREEN:
+        window = pygame.display.set_mode(pygame.display.list_modes()[0], pygame.FULLSCREEN)
+    else:
+        window = pygame.display.set_mode((WIN_W, WIN_H), 0)
+
+def controls():
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            return False
+
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_ESCAPE:
+                return False
+
+            if e.key in KEYS_LEFT:
+                actions.append(('move-left', ownId))
+            if e.key in KEYS_RIGHT:
+                actions.append(('move-right', ownId))
+            if e.key in KEYS_UP:
+                actions.append(('move-up', ownId))
+            if e.key in KEYS_DOWN:
+                actions.append(('move-down', ownId))
+
+            if e.key in KEYS_FIRE:
+                actions.append(('fire-press', ownId))
+
+            if e.key == pygame.K_RETURN:
+                mods = pygame.key.get_mods()
+                if mods & pygame.KMOD_LALT or mods & pygame.KMOD_RALT:
+                    toggleFullscreen()
+
+        if e.type == pygame.KEYUP:
+            if e.key in KEYS_LEFT:
+                actions.append(('stop-left', ownId))
+            if e.key in KEYS_RIGHT:
+                actions.append(('stop-right', ownId))
+            if e.key in KEYS_UP:
+                actions.append(('stop-up', ownId))
+            if e.key in KEYS_DOWN:
+                actions.append(('stop-down', ownId))
+
+            if e.key in KEYS_FIRE:
+                actions.append(('fire-release', ownId))
+
+            if e.key == pygame.K_F11:
+                global FPS
+                if FPS == 20:
+                    FPS = 60
+                else:
+                    FPS = 20
+
+            if e.key == pygame.K_F12:
+                global DEBUG_MODE
+                DEBUG_MODE = not DEBUG_MODE
+
+        if e.type == pygame.JOYAXISMOTION:
+            if e.axis == 0:
+                if e.value < -JOY_DEADZONE:
+                    actions.append(('move-left', ownId))
+                elif e.value > JOY_DEADZONE:
+                    actions.append(('move-right', ownId))
+                else:
+                    if ownPlayer.xdir < 0:
+                        actions.append(('stop-left', ownId))
+                    if ownPlayer.xdir > 0:
+                        actions.append(('stop-right', ownId))
+
+            if e.axis == 1:
+                if e.value < -JOY_DEADZONE:
+                    actions.append(('move-up', ownId))
+                elif e.value > JOY_DEADZONE:
+                    actions.append(('move-down', ownId))
+                else:
+                    if ownPlayer.ydir < 0:
+                        actions.append(('stop-up', ownId))
+                    if ownPlayer.ydir > 0:
+                        actions.append(('stop-down', ownId))
+
+        if e.type == pygame.JOYBUTTONDOWN:
+            actions.append(('fire-press', ownId))
+
+        if e.type == pygame.JOYBUTTONUP:
+            actions.append(('fire-release', ownId))
+
+    return True
+
+def render():
+    screen.fill((0, 128, 0))
+    if tick < 180:
+        font.drawText(screen, 'GRAVWERK', 2, 2, fgcolor=(255,255,255))#, bgcolor=(0,0,0))
+
+def update():
+    global actions
+
+    clientId = None
+    for action, objId in actions:
+
+        obj = gamestate.objects.get(objId)
+
+        if not obj:
+            continue
+
+        if action == 'move-left':
+            obj.moveLeft()
+        elif action == 'move-right':
+            obj.moveRight()
+        elif action == 'move-up':
+            obj.moveUp()
+        elif action == 'move-down':
+            obj.moveDown()
+        elif action == 'stop-left':
+            obj.stopLeft()
+        elif action == 'stop-right':
+            obj.stopRight()
+        elif action == 'stop-up':
+            obj.stopUp()
+        elif action == 'stop-down':
+            obj.stopDown()
+        elif action == 'fire-press':
+            obj.interact(gamestate)
+        elif action == 'fire-release':
+            obj.interact(gamestate, release=True)
+
+    actions = []
+
+
+def init():
+    pass
+
+
+init()
+
+tick = 0
+running = True
+
+try:
+    while running:
+        tick += 1
+
+        render()
+
+        pygame.transform.scale(screen, window.get_size(), window)
+        pygame.display.flip()
+
+        cont = controls()
+
+        if not cont:
+            running = False
+
+        update()
+
+
+        clock.tick(FPS)
+
+finally:
+    pygame.quit()
+
---
+++ globalconst.py
@@ -0,0 +1,42 @@
+import pygame
+
+SCR_W = 320
+SCR_H = 180
+
+WIN_W = 1280
+WIN_H = 720
+
+TILE_W = 8
+TILE_H = 8
+
+LEV_W = 40
+LEV_H = 22
+
+FPS = 60
+
+LEFT = 0
+RIGHT = 1
+UP = 2
+DOWN = 3
+
+FULLSCREEN = False
+DEBUG_MODE = False
+JOY_DEADZONE = 0.4
+
+REDUCE_DIAGONAL_SPEED = True
+VARIABLE_KICK_POWER = True
+
+WORM_MOVE_TIME = 0.2
+WORM_STEPS_TIL_ADDITION = 0
+WORM_INITIAL_GROW = 16
+
+PLAYER_RESPAWN_TIME = 1.5   # in seconds
+WORM_RESPAWN_TIME = 1.5
+BALL_RESPAWN_TIME = 1.0
+BALL_RESPAWN_TIME_AFTER_GOAL = 2.5
+
+KEYS_LEFT = [pygame.K_LEFT, pygame.K_a]
+KEYS_RIGHT = [pygame.K_RIGHT, pygame.K_d]
+KEYS_UP = [pygame.K_UP, pygame.K_w]
+KEYS_DOWN = [pygame.K_DOWN, pygame.K_s]
+KEYS_FIRE = [pygame.K_LCTRL, pygame.K_SPACE]
